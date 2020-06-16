fd32155c1b1d0ed4d9e268660348e6f6257b115c
Tony <hjaldrsonn@gmail.com>
2020-06-16 18:08:18 +0300

    16.06.20

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-;jkgh;khn
-console.log()
+// Rest param
+function max(a,b,... nums) {
+    console.log(nums)
+}
+
+// max(1,5,7);
 
 
+// Spread operator
+const arr = [1, 2, 3];
+const str ='haFSFAui pizFASFda lapouSFASfhaya';
 
+function hui (str) {
+    const asfa= [...str];
+    console.log((asfa));
+    const arr = str.toLowerCase()
+    .split(' ')
+    .map(el => el[0].toUpperCase() + el.slice(1));
+    
+    return arr;
+}
 
-lkln
-
-console.log()
+console.log(hui(str));
